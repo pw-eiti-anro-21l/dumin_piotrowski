9209176019687b45c387e8b73e88cad60fa694e1
MateuszD <mateusz.dumin.stud@pw.edu.pl>
2021-05-06 17:03:22 +0200

    x

---
+++ my_robot/my_robot/NONKDL_DKIN.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_robot.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ my_robot/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'my_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='medivh',
+    maintainer_email='mateusz.dumin.stud@pw.edu.pl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'state_publisher = my_robot.state_publisher:main'
+        ],
+    },
+)
