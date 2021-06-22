d86dc6f79bcb333f3c238ee84190b69b7b98d64e
Bartomiej Piotrowski <ksordas@gmail.com>
2021-06-22 03:23:40 +0200

    projekt3

---
+++ cpp_my_robot/launch/my_robot_KDL.launch.py
@@ -0,0 +1,52 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'my_robot.urdf.xml'
+    urdf = os.path.join(
+        get_package_share_directory('cpp_my_robot'),
+        urdf_file_name)
+        
+    rviz_file_name = 'my_robot.rviz'
+    rviz_path = os.path.join(
+        get_package_share_directory('cpp_my_robot'),
+        rviz_file_name)
+        
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': 	robot_desc}],
+            arguments=[urdf]),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-d', rviz_path]),   
+	 Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'),
+         Node(
+		 	 package='cpp_my_robot',
+			 executable='KDL_DKIN',
+			 name='KDL_DKIN')
+    ])
